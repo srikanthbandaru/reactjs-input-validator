fc4948211f313c74eb77ac5f49fb0cafd03caa3c
Srikanth Bandaru <srikanthb602@gmail.com>
2018-02-10 18:34:50 -0600

    A super ugly input validator for type email

---
+++ src/Input.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+import validator from 'validator'
+import {errorMessages} from './errorMessages'
+
+export default class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''}
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleOnFocus = this.handleOnFocus.bind(this)
+  }
+
+  handleInputChange(event) {
+    this.setState({value: event.target.value})
+    if(this.props.validator) {
+      const validationResult = validator[this.props.validator](this.state.value)
+      console.log(validationResult);
+      if(!validationResult) {
+        console.log(errorMessages.isEmail);
+        console.log(errorMessages[this.props.validator]);
+      }
+    }
+  }
+
+  handleOnFocus() {
+    if (this.props.id !== document.activeElement.id) {
+      const validator = this.props.validator;
+      console.log(this.props.validator);
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <input
+          type='text'
+          id={this.props.id}
+          name="reactjs-input-validator"
+          value={this.state.value}
+          onChange={this.handleInputChange}
+          onFocus={this.handleOnFocus}
+        />
+      </div>
+    )
+  }
+}
---
+++ src/errorMessages.js
@@ -0,0 +1,3 @@
+export const errorMessages = {
+  isEmail: 'Please enter a valid email address'
+}
