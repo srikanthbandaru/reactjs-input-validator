da3559516fef3d1fc6ba34257c51ddbe35441c86
Srikanth Bandaru <srikanthb602@gmail.com>
2018-03-08 20:20:36 -0600

    v0.14.2

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+// import { Row, Col } from 'react-bootstrap';
 import './App.css';
 import { Input, formInputData, formValidation } from '../lib';
 
@@ -35,7 +35,7 @@ export default class App extends Component {
 
     if (isFormValid) {
       // do something
-      this.setState({ callAPI: true });
+      // this.setState({ callAPI: true });
     } else {
       // to do validation and display error msgs for all Inputs in the form
       Object.keys(this.state.data).map(input => [this[input].focus(), this[input].blur()]);
@@ -43,163 +43,16 @@ export default class App extends Component {
   }
 
   render() {
-    const passwordValue = this.state.data.password && this.state.data.password.value
-      ? this.state.data.password.value
-      : null;
+    // const passwordValue = this.state.data.password && this.state.data.password.value
+    //   ? this.state.data.password.value
+    //   : null;
 
     return (
       <form className="example">
-        <Row>
-          <Col md={6}>
-
-            {/*
-              Input required validation check with
-              library default error messages
-            */}
-            <Input
-              required
-              label="Full Name" name="fullName" placeholder="First Last"
-              onChange={this.handleChange}
-              setRef={(input) => { this.fullName = input; }}
-            />
-
-          </Col>
-          <Col md={6}>
-
-            {/*
-              Input required validation check with
-              isEmail validation and
-              library default error messages
-            */}
-            <Input
-              validator="isEmail" required
-              label="Email" name="email" placeholder="Email"
-              onChange={this.handleChange}
-              setRef={(input) => { this.email = input; }}
-            />
-
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-
-            {/*
-              Input required validation check with
-              isAlphanumeric validation
-              and minimum character length validation with
-              custom error msg only when minLength validation fail
-            */}
-            <Input
-              validator="isAlphanumeric" required minLength={8}
-              minLengthErrMsg="Short passwords are easy to guess. Try one with atleast 8 characters"
-              label="Create a password" name="password" type="password" placeholder="Password"
-              onChange={this.handleChange}
-              setRef={(input) => { this.password = input; }}
-            />
-
-          </Col>
-          <Col md={6}>
-
-            {/*
-              Input required validation check with
-              equals validation with
-              custom error msg only when equals validation fail
-            */}
-            <Input
-              validator="equals" required comparison={passwordValue}
-              validatorErrMsg="These passwords don't match. Try again?"
-              label="Confirm password" name="confirmPassword" type="password" placeholder="Password"
-              onChange={this.handleChange}
-              setRef={(input) => { this.confirmPassword = input; }}
-            />
-
-          </Col>
-        </Row>
-
-        {/*
-          Input required validation check with
-          custom error msg only when required validation fail
-        */}
         <Input
-          required
-          requiredErrMsg="Enter your address so we can send you awesome stuff"
-          label="Address" name="address" placeholder="1234 Main St"
-          onChange={this.handleChange}
-          setRef={(input) => { this.address = input; }}
+          validator="isEmail" required
+          label="Email" name="email" placeholder="Email"
         />
-
-        {/*
-          No validation
-        */}
-        <Input
-          label="Address 2"
-          name="address2" placeholder="Apartment, studio, or floor"
-          onChange={this.handleChange}
-          setRef={(input) => { this.address2 = input; }}
-        />
-
-        <Row>
-          <Col md={6}>
-
-            {/*
-              Input required validation check with
-              maximum character length validation with
-              library default error messages
-            */}
-            <Input
-              maxLength={20} required label="City"
-              name="inputCity"
-              onChange={this.handleChange}
-              setRef={(input) => { this.inputCity = input; }}
-            />
-
-          </Col>
-          <Col md={3}>
-
-            {/*
-              You can declare other input types too
-            */}
-            <label htmlFor="inputState">State</label>
-            <select
-              name="inputState" className="form-control"
-              onChange={this.handleChange}
-              value={this.state.data.inputState ? this.state.data.inputState.value : ''}
-            >
-              <option>Choose...</option>
-              <option value="ALABAMA">ALABAMA</option>
-              <option value="ALASKA">ALASKA</option>
-              <option value="ARIZONA">ARIZONA</option>
-              <option>...</option>
-            </select>
-
-          </Col>
-          <Col md={3}>
-
-            {/*
-              Input required validation check with
-              maximum character length validation with
-              library default error messages
-            */}
-            <Input
-              validator="isPostalCode" locale="US" required maxLength={10}
-              validatorErrMsg="Enter a valid US Zip"
-              label="Zip" name="inputZip"
-              onChange={this.handleChange}
-              setRef={(input) => { this.inputZip = input; }}
-            />
-          </Col>
-        </Row>
-        <button
-          type="submit" onClick={this.handleSubmit} className="btn btn-primary"
-        >Sign up
-        </button>
-        {this.state.callAPI
-          ?
-            <pre className="resultBlock">
-              {JSON.stringify(formInputData(this.state.data), null, 4)}
-            </pre>
-          : null
-        }
       </form>
     );
   }
