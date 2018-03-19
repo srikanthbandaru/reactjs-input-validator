5ff44268ecc71e743ebb89f3ac0093dfbd8cba2e
Srikanth Bandaru <srikanthb602@gmail.com>
2018-03-18 23:12:18 -0500

    Polyfill Array.includes

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,6 @@
+// polyfills
+import 'core-js/fn/array/includes';
+
 import Input from './components/Input';
 import formInputData from './utils/formInputData';
 import formValidation from './utils/formValidation';
